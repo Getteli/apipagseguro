27f5b3746f724c4875798bca368c8bd2f19dabeb
Getteli <douglas.araujo.01@outlook.com>
2023-05-21 12:07:34 -0300

    fixed tokens exposed

--- Legacy - PAGSEGURO - Checkout transparente/_assets/pagseguro/config.php
+++ Legacy - PAGSEGURO - Checkout transparente/_assets/pagseguro/config.php
@@ -1,5 +1,5 @@
 <?php
 // credenciais do pagseguro da conta do nosso cliente.
-define("EMAIL_PAGSEGURO","iliontecnologia@gmail.com");
-define("TOKEN_PAGSEGURO","8A8827FB83BD4E318FB5FC99779A251B"); // token producao
-define("TOKEN_SANDBOX","0FF1832C6D9046A9A9925A6D06DD08F6"); // token sandbox / ambiente de teste
+define("EMAIL_PAGSEGURO","seuemailpagseguro@gmail.com");
+define("TOKEN_PAGSEGURO",""); // token producao
+define("TOKEN_SANDBOX",""); // token sandbox / ambiente de teste
--- Legacy - PAGSEGURO - Fixo/_assets/pagseguro/config.php
+++ Legacy - PAGSEGURO - Fixo/_assets/pagseguro/config.php
@@ -1,8 +1,8 @@
 <?php
 // credenciais do pagseguro da conta do nosso cliente.
-define("EMAIL_PAGSEGURO","iliontecnologia@gmail.com");
-define("TOKEN_PAGSEGURO","8A8827FB83BD4E318FB5FC99779A251B"); // token producao
-define("TOKEN_SANDBOX","0FF1832C6D9046A9A9925A6D06DD08F6"); // token sandbox / ambiente de teste
+define("EMAIL_PAGSEGURO","seuemailpagseguro@gmail.com");
+define("TOKEN_PAGSEGURO",""); // token producao
+define("TOKEN_SANDBOX",""); // token sandbox / ambiente de teste
 // credenciais pre definidas
 define("CREDENTIALS_PAGSEGURO","email=".EMAIL_PAGSEGURO."&token=".TOKEN_PAGSEGURO); // pagseguro
 define("CREDENTIALS_SANDBOX","email=".EMAIL_PAGSEGURO."&token=".TOKEN_SANDBOX); // sandbox
